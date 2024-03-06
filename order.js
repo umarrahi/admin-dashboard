f4a2fc5b4c5e37516b65094e563019275a7bb5cf
Umar Rahi <125821878+umarrahi@users.noreply.github.com>
2024-03-06 23:21:27 +0500

    added index.html

---
+++ order.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName: 'LARVENDER KF102 Dron',
+        productNumber: '53753',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ruko F11 Pro Dron',
+        productNumber: '79392',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Dron With Camera Dron',
+        productNumber: '83722',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'GPS 4k Dron',
+        productNumber: '46221',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Four Wings Dron',
+        productNumber: '53453',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    }
+]
